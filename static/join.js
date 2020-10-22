92c5a8ecfafbd373c0c8291eac116aa83eced030
unknown <erdosancsa@gmail.com>
2020-10-22 13:41:38 +0200

    Updated Join now updates navbar.

--- static/join.js
+++ static/join.js
@@ -1,13 +1,3 @@
-/*$("#new-household-id").change(function(){
-    console.log("alma");
-    if(this.checked){
-        $("#household-id").prop('readonly', true);
-    }
-    else{
-        $("#household-id").prop('readonly', false);
-    }
-});*/
-
 $(document).on('change', '#new-household-id', function() {
     if(this.checked){
         $("#household-id").prop('readonly', true);
--- static/login.js
+++ static/login.js
@@ -1,49 +1,21 @@
-/*var isloggedIn = true;
-
-var loggedIna = document.getElementsByClassName("nav-link logged-ina");
-if(isloggedIn){
-    console.log(loggedIna.item(1));
-}*/
-/*for (let i = 0; i <= loggedIna.length; i++) {
-    console.log(i);
-    loggedIna.item(i).style.display = "inline";
-    console.log(loggedIna.item(i));
-}*/
-/*var loggedIns = document.getElementsByName("logged-ins");
-var loggedOuta = document.getElementsByName("logged-outa");
-var loggedOuts = document.getElementsByName("logged-outs");
-
-console.log(loggedIns);
-console.log(loggedOuta);
-console.log(loggedOuts);
-
-if (isloggedIn) {
-    console.log(isloggedIn)
-    for (let i = 0; i < loggedIna.length; i++) {
-        loggedIna.item(i).style.display = "inline";
-        console.log(loggedIna.item(i))
-    }
-    for (let j = 0; j < loggedIns.length; j++) {
-        loggedIns.item(j).style.display = "inline";
-    }
-    for (let k = 0; k < loggedOuta.length; k++) {
-        loggedOuta.item(k).style.display = "none";
-    }
-    for (let l = 0; l < loggedOuts.length; l++) {
-        loggedOuts.item(l).style.display = "none";
+/*var logInObj ={
+    isloggedIn: false,
+    isLoggedInListener: function(bool){},
+    set isLoggedInSet(bool){
+        this.isloggedIn = bool;
+        this.isLoggedInListener(bool);
+    },
+    registerListener: function(listener){
+        this.isLoggedInListener = listener;
     }
 }
-else {
-    for (let i = 0; i < loggedIna.length; i++) {
-        loggedIna.item(i).style.display = "none";
-    }
-    for (let j = 0; j < loggedIns.length; j++) {
-        loggedIns.item(j).style.display = "none";
-    }
-    for (let k = 0; k < loggedOuta.length; k++) {
-        loggedOuta.item(k).style.display = "inline";
-    }
-    for (let l = 0; l < loggedOuts.length; l++) {
-        loggedOuts.item(l).style.display = "inline";
-    }
-}*/
+
+logInObj.registerListener(function(bool){
+    $('')
+})*/
+
+$(document).on('click', '#join-now', function(){
+    //isloggedIn =true;
+    $('.logged-in').css('display','inline');
+    $('.logged-out').css('display','none');
+})
