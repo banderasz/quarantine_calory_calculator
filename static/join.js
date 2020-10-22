77b603961ed8740ea72798b4a9441457a8ebd647
unknown <erdosancsa@gmail.com>
2020-10-22 12:28:08 +0200

    Updated Join now site.

--- static/join.js
+++ static/join.js
@@ -1,1 +1,18 @@
-$()
+/*$("#new-household-id").change(function(){
+    console.log("alma");
+    if(this.checked){
+        $("#household-id").prop('readonly', true);
+    }
+    else{
+        $("#household-id").prop('readonly', false);
+    }
+});*/
+
+$(document).on('change', '#new-household-id', function() {
+    if(this.checked){
+        $("#household-id").prop('readonly', true);
+    }
+    else{
+        $("#household-id").prop('readonly', false);
+    }
+});
