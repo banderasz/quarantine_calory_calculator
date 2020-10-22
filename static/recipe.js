337754fba5b35217a77e09258d5adbbd0058ebe4
unknown <erdosancsa@gmail.com>
2020-10-22 23:54:35 +0200

    Profile and Contact us site UI-s are empty. The rest is works nice

--- static/recipe.js
+++ static/recipe.js
@@ -37,3 +37,40 @@ $(document).on('click', '#recipes-type5', function() {
     $('#recipes-type4').css('font-weight','normal');
     $('#recipes-type1').css('font-weight','normal');
 });
+
+$(document).on('click', '#add-ingredient-btn', function(){
+    $('#ingredient-table tbody:last-child').append($('<tr>')
+                .append($('<td>')
+                    .append($('<input>')
+                        .attr('class', 'form-control form-control-sm')
+                        .attr('placeholder','Ingredient name')
+                        .attr('type','text')
+                    )
+                )
+                .append($('<td>')
+                    .append($('<div>')
+                        .attr('class', 'input-group input-group-sm')
+                        .append($('<input>')
+                            .attr('type', 'text')
+                            .attr('class', 'form-control')
+                        )
+                        .append($('<div>')
+                            .attr('class', 'input-group-append')
+                            .append($('<span>')
+                                .attr('class', 'input-group-text')
+                                .text('g')
+                            )
+                        )
+                    )
+                )
+            )
+})
+
+$(document).on('click', '#add-step-btn', function(){
+    $('#steps-list').append($('<li>')
+                .append($('<textarea>')
+                    .attr('class', 'form-control')
+                    .attr('rows','3')
+                )
+            )
+})
