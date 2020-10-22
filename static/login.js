a1ade0328b9098b89c93d58d081760cef7297408
unknown <erdosancsa@gmail.com>
2020-10-22 11:30:47 +0200

    First version of Front End.

---
+++ static/login.js
@@ -0,0 +1,49 @@
+/*var isloggedIn = true;
+
+var loggedIna = document.getElementsByClassName("nav-link logged-ina");
+if(isloggedIn){
+    console.log(loggedIna.item(1));
+}*/
+/*for (let i = 0; i <= loggedIna.length; i++) {
+    console.log(i);
+    loggedIna.item(i).style.display = "inline";
+    console.log(loggedIna.item(i));
+}*/
+/*var loggedIns = document.getElementsByName("logged-ins");
+var loggedOuta = document.getElementsByName("logged-outa");
+var loggedOuts = document.getElementsByName("logged-outs");
+
+console.log(loggedIns);
+console.log(loggedOuta);
+console.log(loggedOuts);
+
+if (isloggedIn) {
+    console.log(isloggedIn)
+    for (let i = 0; i < loggedIna.length; i++) {
+        loggedIna.item(i).style.display = "inline";
+        console.log(loggedIna.item(i))
+    }
+    for (let j = 0; j < loggedIns.length; j++) {
+        loggedIns.item(j).style.display = "inline";
+    }
+    for (let k = 0; k < loggedOuta.length; k++) {
+        loggedOuta.item(k).style.display = "none";
+    }
+    for (let l = 0; l < loggedOuts.length; l++) {
+        loggedOuts.item(l).style.display = "none";
+    }
+}
+else {
+    for (let i = 0; i < loggedIna.length; i++) {
+        loggedIna.item(i).style.display = "none";
+    }
+    for (let j = 0; j < loggedIns.length; j++) {
+        loggedIns.item(j).style.display = "none";
+    }
+    for (let k = 0; k < loggedOuta.length; k++) {
+        loggedOuta.item(k).style.display = "inline";
+    }
+    for (let l = 0; l < loggedOuts.length; l++) {
+        loggedOuts.item(l).style.display = "inline";
+    }
+}*/
---
+++ static/size.js
@@ -0,0 +1,31 @@
+var lastWidth = $(window).width();
+var navText = document.getElementsByClassName("navbar-text hide");
+$(window).ready(function(){
+    if (lastWidth < 992) {
+        for (let i = 0; i < navText.length; i++) {
+            navText.item(i).style.display = "none";
+            console.log(navText.item(i));
+        }
+    }
+    else {
+        for (var i = 0; i < navText.length; i++) {
+            navText.item(i).style.display = "inline";
+        }
+    }
+})
+$(window).resize(function () {
+    if ($(window).width() != lastWidth) {
+        lastWidth = $(window).width();
+        if (lastWidth < 992) {
+            for (let i = 0; i < navText.length; i++) {
+                navText.item(i).style.display = "none";
+            }
+        }
+        else {
+            for (var i = 0; i < navText.length; i++) {
+                navText.item(i).style.display = "inline";
+            }
+        }
+    }
+})
+
