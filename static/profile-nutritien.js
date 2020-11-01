4a0bf75218492ab1106c312f3a1f47fa0cc36fc9
unknown <erdosancsa@gmail.com>
2020-11-01 23:56:11 +0100

    Personal site is working

--- static/profile-nutritien.js
+++ static/profile-nutritien.js
@@ -67,6 +67,30 @@ $(document).ready(function () {
     $(".personal").hide();
 });
 
+//Nav
+$(document).on('click', '#nutin-nav', function () {
+    $('#nutin-nav').css('font-weight', '500');
+    $('#weight-nav').css('font-weight', 'normal');
+    $('#house-nav').css('font-weight', 'normal');
+    $('#sport-nav').css('font-weight', 'normal');
+    $('#personal-nav').css('font-weight', 'normal');
+    $('#nutin-nav').css('font-weight', '500');
+    $(".th-save-cancel").hide();
+    if (parseFloat($('#total-cal-nutin').text()) > parseFloat($('#ideal-cal').text())) {
+        $('#total-row').css('color', 'red');
+        $('.row.alert-row-nutin').show();
+    }
+    else{
+        $('.row.alert-row-nutin').hide();
+    }
+    $(".nutritien-intake").show();
+    $(".household-stock").hide();
+    $(".sport").hide();
+    $(".personal").hide();
+    $('.row.alert-row-house').hide();
+    $('.row.alert-row-sport').hide();
+});
+
 //Nutritien intake
 $(document).on('click', '.th-add-new-food-nutin', function () {
     $('#nutritien-intake-tbody').append($('<tr>')
